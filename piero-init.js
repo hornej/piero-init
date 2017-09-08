dd4b6126e42c5c348ae5dcbca94948ff61f71ca3
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 00:47:04 -0600

    test for failure

--- piero-init.js
+++ piero-init.js
@@ -1,1 +1,4 @@
 const childProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
+
+const fs = require('fs');
+fs.writeFileSync('/home/chip/it-worked', 'it worked');
