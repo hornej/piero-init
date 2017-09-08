832cb7bc5368be75d245057ee1554aa41578420f
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 00:36:14 -0600

    test

--- piero-init.js
+++ piero-init.js
@@ -1,1 +1,1 @@
-const childProcess = require('child_process').spawn('python /home/chip/piero/chip_scan.py');
+const childProcess = require('child_process').spawn('touch /home/chip/it-worked && python /home/chip/piero/chip_scan.py');
