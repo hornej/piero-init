19876ec24f8c49d16a954a7d86e77a674809a7c6
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 01:13:51 -0600

    get the cron job to run at the 15 second mark

--- piero-init.js
+++ piero-init.js
@@ -2,9 +2,8 @@ const schedule = require('node-schedule');
 const fs = require('fs');
 
 const childProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
-fs.writeFileSync('/home/chip/python-process-started', 'it worked');
 
-schedule.scheduleJob(`0 2 1 0 0 0`, () => {
+schedule.scheduleJob(`15 * * * * *`, () => {
     childProcess.kill('SIGINT');
     fs.writeFileSync('/home/chip/python-process-killed', 'it worked');
 });
