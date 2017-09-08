66750fcf1d026b34ba9cd7d5d81878c6797e5d90
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 13:21:40 -0600

    comment out email tests

--- piero-init.js
+++ piero-init.js
@@ -1,30 +1,31 @@
 const schedule = require('node-schedule');
 const fs = require('fs');
-const nodemailer = require('nodemailer');
-const SMTPServer = require('smtp-server');
-const smtpServer = new SMTPServer();
-const smtpServerPort = 25;
-smtpServer.listen(smtpServerPort);
-const smtpTransport = require('nodemailer-smtp-transport');
-const transporter = nodemailer.createTransport(smtpTransport({
-    host: 'localhost',
-    port: smtpServerPort,
-    auth: {
-        user: 'username',
-        pass: 'password'
-    }
-}));
-const childProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
+// const nodemailer = require('nodemailer');
+// const SMTPServer = require('smtp-server').SMTPServer;
+// const smtpServer = new SMTPServer();
+// const smtpServerPort = 8080;
+// smtpServer.listen(smtpServerPort);
+// const smtpTransport = require('nodemailer-smtp-transport');
+// const transporter = nodemailer.createTransport(smtpTransport({
+//     host: 'localhost',
+//     port: smtpServerPort,
+//     auth: {
+//         user: 'username',
+//         pass: 'password'
+//     }
+// }));
+// const childProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
 
 schedule.scheduleJob({
     second: 5
 }, () => {
-    childProcess.kill('SIGINT');
-    fs.writeFileSync(`/home/chip/python-process-killed-${new Date()}`, 'it worked');
-    transporter.sendMail({
-        from: 'robot@fake-address.com',
-        to: 'jordan.michael.last@gmail.com',
-        subject: 'Test',
-        text: 'This is a test'
-    });
+    // childProcess.kill('SIGINT');
+    // fs.writeFileSync(`/home/chip/python-process-killed-${new Date()}`, 'it worked');
+    // console.log('sending email');
+    // transporter.sendMail({
+    //     from: 'robot@fake-address.com',
+    //     to: 'jordan.michael.last@gmail.com',
+    //     subject: 'Test',
+    //     text: 'This is a test'
+    // });
 });
