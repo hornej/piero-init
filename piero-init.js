7f8a71f671707fa568250af66eac0fa6e20d3cda
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 00:17:54 -0600

    simplify test

--- piero-init.js
+++ piero-init.js
@@ -1,5 +1,2 @@
 const fs = require('fs');
-
-setInterval(() => {
-    fs.writeFileSync(`/home/chip/test-${new Date()}`, new Date());
-}, 1000);
+fs.writeFileSync(`/home/chip/test-${new Date()}`, new Date());
