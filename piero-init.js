4083ac36079a7e74fc141e14425b60f34127c44a
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 01:32:05 -0600

    try at the 5 second mark every minute, and send an email

--- piero-init.js
+++ piero-init.js
@@ -1,12 +1,19 @@
 const schedule = require('node-schedule');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport();
 const childProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
 
 schedule.scheduleJob({
-    hour: 3,
-    minute: 25
+    second: 5
 }, () => {
     childProcess.kill('SIGINT');
-    fs.writeFileSync('/home/chip/python-process-killed', 'it worked');
+    fs.writeFileSync(`/home/chip/python-process-killed-${new Date()}`, 'it worked');
+    transporter.sendMail({
+        from: 'robot@fake-address.com',
+        to: 'jordan.michael.last@gmail.com',
+        subject: 'Test',
+        text: 'This is a test'
+    });
 });
