484f6e4b5581af71b026dff620183bc92cfd6818
Jordan Last <jordan.michael.last@gmail.com>
2017-09-07 23:30:23 -0600

    updates

--- piero-init.sh
+++ piero-init.sh
@@ -1,15 +1,22 @@
-echo "Cloning piero-init repository not implemented"
-echo "Cloning piero repository not implemented"
+echo "Move to /home/chip"
+cd /home/chip
+
+echo "Cloning piero-init repository"
+git clone https://github.com/lastmjs/piero-init.git
+
+echo "Cloning piero repository"
+git clone https://github.com/hornej/piero.git
 
 echo "Downloading nvm"
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 
-echo "Resetting the terminal for nvm not implemented"
+# echo "Resetting the terminal for nvm not implemented"
 
-echo "Install Node.js v8.4.0"
-nvm install 8.4.0
+# echo "Install Node.js v8.4.0"
+# nvm install 8.4.0
 
 echo "Copying piero-init.service to /etc/systemd/system directory"
+cd piero-init
 cp piero-init.service /etc/systemd/system
 
 echo "Enable piero-init.service on startup"
