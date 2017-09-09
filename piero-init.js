0d19bc03242ab2b756053e39fd46be0f67897c39
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 19:00:51 -0600

    just run the whole init script again...the device will reboot itself twice per day, but will be able to update all of its code remotely, even the remote script

--- piero-init.js
+++ piero-init.js
@@ -2,45 +2,52 @@ const schedule = require('node-schedule');
 const fs = require('fs');
 const git = require('simple-git')('../piero');
 
-let childProcess = startPythonProcess();
+const pythonProcess = startPythonProcess();
 
 schedule.scheduleJob({
     second: 5
-}, performPullRoutine);
+}, () => {
+    //childProcess.kill('SIGINT'); //TODO we might want to kill the python process...maybe not though...probably not
+    require('child_process').execSync('wget -qO- https://raw.githubusercontent.com/hornej/piero-init/master/piero-init.sh | bash');
+});
 
-schedule.scheduleJob({
-    second: 35
-}, performPullRoutine);
-
-function performPullRoutine() {
-    git.silent(true).pull('origin', 'master', (error, data) => {
-        if (error) {
-            //TODO send out failure message
-            fs.writeFileSync(`/home/chip/git-pull-error-${new Date()}`, error);
-            return;
-        }
-
-        if (data) {
-            //TODO send out success message
-            childProcess.kill('SIGINT'); //TODO I want to be sure the process is killed
-            childProcess = startPythonProcess();
-            fs.writeFileSync(`/home/chip/git-pull-success-${new Date()}`, data);
-            return;
-        }
-    });
-}
-
+// schedule.scheduleJob({
+//     second: 5
+// }, performPullRoutine);
+//
+// schedule.scheduleJob({
+//     second: 35
+// }, performPullRoutine);
+//
+// function performPullRoutine() {
+//     git.silent(true).pull('origin', 'master', (error, data) => {
+//         if (error) {
+//             //TODO send out failure message
+//             fs.writeFileSync(`/home/chip/git-pull-error-${new Date()}`, error);
+//             return;
+//         }
+//
+//         if (data) {
+//             //TODO send out success message
+//             childProcess.kill('SIGINT'); //TODO I want to be sure the process is killed
+//             childProcess = startPythonProcess();
+//             fs.writeFileSync(`/home/chip/git-pull-success-${new Date()}`, data);
+//             return;
+//         }
+//     });
+// }
+//
 function startPythonProcess() {
-    const childProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
-    childProcess.on('error', (error) => {
+    const pythonProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
+    pythonProcess.on('error', (error) => {
         fs.writeFileSync(`/home/chip/child-process-error-${new Date()}`, error);
     });
-    childProcess.stdout.on('data', (data) => {
+    pythonProcess.stdout.on('data', (data) => {
         fs.writeFileSync(`/home/chip/child-process-stdout-${new Date()}`, data);
     });
-    childProcess.stderr.on('data', (data) => {
+    pythonProcess.stderr.on('data', (data) => {
         fs.writeFileSync(`/home/chip/child-process-stderr-${new Date()}`, data);
     });
     fs.writeFileSync(`/home/chip/child-process-started-${new Date()}`, 'child-process-started');
-    return childProcess;
+    return pythonProcess;
 }
