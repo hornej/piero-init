c2cee70202127e6293dfd44393510cdd813eca54
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 19:29:00 -0600

    send messages to graphcool

--- piero-init.js
+++ piero-init.js
@@ -1,51 +1,60 @@
 const schedule = require('node-schedule');
-const fs = require('fs');
-const pythonProcess = startPythonProcess();
+// const fs = require('fs');
+const fetch = require('isomorphic-fetch')
+
+startPythonProcess();
 
 schedule.scheduleJob({
     second: 5
 }, () => {
-    //childProcess.kill('SIGINT'); //TODO we might want to kill the python process...maybe not though...probably not
     require('child_process').execSync('wget -qO- https://raw.githubusercontent.com/hornej/piero-init/master/piero-init.sh | bash');
 });
 
-// schedule.scheduleJob({
-//     second: 5
-// }, performPullRoutine);
-//
-// schedule.scheduleJob({
-//     second: 35
-// }, performPullRoutine);
-//
-// function performPullRoutine() {
-//     git.silent(true).pull('origin', 'master', (error, data) => {
-//         if (error) {
-//             //TODO send out failure message
-//             fs.writeFileSync(`/home/chip/git-pull-error-${new Date()}`, error);
-//             return;
-//         }
-//
-//         if (data) {
-//             //TODO send out success message
-//             childProcess.kill('SIGINT'); //TODO I want to be sure the process is killed
-//             childProcess = startPythonProcess();
-//             fs.writeFileSync(`/home/chip/git-pull-success-${new Date()}`, data);
-//             return;
-//         }
-//     });
-// }
-//
 function startPythonProcess() {
     const pythonProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
     pythonProcess.on('error', (error) => {
-        fs.writeFileSync(`/home/chip/child-process-error-${new Date()}`, error);
+        // fs.writeFileSync(`/home/chip/child-process-error-${new Date()}`, error);
+        sendMessage(`python process error: ${error}`);
     });
     pythonProcess.stdout.on('data', (data) => {
-        fs.writeFileSync(`/home/chip/child-process-stdout-${new Date()}`, data);
+        // fs.writeFileSync(`/home/chip/child-process-stdout-${new Date()}`, data);
+        sendMessage(`python process stdout: ${data}`);
     });
     pythonProcess.stderr.on('data', (data) => {
-        fs.writeFileSync(`/home/chip/child-process-stderr-${new Date()}`, data);
+        // fs.writeFileSync(`/home/chip/child-process-stderr-${new Date()}`, data);
+        sendMessage(`python process stderr: ${data}`);
     });
-    fs.writeFileSync(`/home/chip/child-process-started-${new Date()}`, 'child-process-started');
+    sendMessage(`python process started`);
+    // fs.writeFileSync(`/home/chip/child-process-started-${new Date()}`, 'child-process-started');
     return pythonProcess;
+}
+
+function sendMessage(message) {
+    fetch('https://api.graph.cool/simple/v1/cj7cmhity0g5u0108ta01f6lt', {
+          method: 'post',
+          headers: {
+              'content-type': 'application/json'
+          },
+          body: JSON.stringify({
+              query: `
+                mutation createMessage($message: String!) {
+                	createMessage(
+                		message: $message
+                	) {
+                		id
+                	}
+                }
+              `,
+              variables: {
+                  message
+              }
+          })
+      })
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      const errors = responseJSON.errors;
+      if (errors) {
+          console.log(errors);
+      }
+    });
 }
