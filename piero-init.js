2fa850ad49464c9908d047497f46645aff9d6a87
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 22:09:32 -0600

    using s3

--- piero-init.js
+++ piero-init.js
@@ -13,50 +13,26 @@ schedule.scheduleJob({
 function startPythonProcess() {
     const pythonProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
     pythonProcess.on('error', (error) => {
-        // fs.writeFileSync(`/home/chip/child-process-error-${new Date()}`, error);
         sendMessage(`python process error: ${error}`);
     });
     pythonProcess.stdout.on('data', (data) => {
-        // fs.writeFileSync(`/home/chip/child-process-stdout-${new Date()}`, data);
         sendMessage(`python process stdout: ${data}`);
     });
     pythonProcess.stderr.on('data', (data) => {
-        // fs.writeFileSync(`/home/chip/child-process-stderr-${new Date()}`, data);
         sendMessage(`python process stderr: ${data}`);
     });
+
     sendMessage(`python process started`);
-    fs.writeFileSync(`/home/chip/child-process-started-${new Date()}`, 'child-process-started'); //TODO get rid of this
+
     return pythonProcess;
 }
 
 function sendMessage(message) {
-    fetch('https://api.graph.cool/simple/v1/cj7cmhity0g5u0108ta01f6lt', {
-          method: 'post',
-          headers: {
-              'content-type': 'application/json',
-              'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDQ5MjYzODAsImNsaWVudElkIjoiY2oyd2lmdnZmM29raTAxNTRtZnN0c2lscCIsInByb2plY3RJZCI6ImNqN2NtaGl0eTBnNXUwMTA4dGEwMWY2bHQiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqN2NxYjZxOTBycGowMTk4emY0emtzNXgifQ.LrWunzhJlU4lBIgjs7Z6jAaLsVTj9JNvbYEsKgG622Q`
-          },
-          body: JSON.stringify({
-              query: `
-                mutation createMessage($message: String!) {
-                	createMessage(
-                		message: $message
-                	) {
-                		id
-                	}
-                }
-              `,
-              variables: {
-                  message
-              }
-          })
-      })
-    .then((response) => response.json())
-    .then((responseJSON) => {
-      const errors = responseJSON.errors;
-      if (errors) {
-          fs.writeFileSync(`/home/chip/piero-init-error-${new Date()}`, errors);
-      }
+    fetch('https://piero-test.s3.amazonaws.com/message', {
+        method: 'put',
+        body: JSON.stringify({
+            message
+        })
     })
     .catch((error) => {
         fs.writeFileSync(`/home/chip/piero-init-error-${new Date()}`, error);
