327720eccc749b8eb6dda705637c38ba94e9a9fc
Jordan Last <jordan.michael.last@gmail.com>
2017-09-08 18:36:32 -0600

    add more logging

--- piero-init.js
+++ piero-init.js
@@ -10,23 +10,25 @@ schedule.scheduleJob({
     git.silent(true).pull('origin', 'master', (error, data) => {
         if (error) {
             //TODO send out failure message
-            fs.writeFileSync(`/home/chip/error-${new Date()}`, error);
+            fs.writeFileSync(`/home/chip/git-pull-error-${new Date()}`, error);
             return;
         }
 
         if (data) {
-            console.log('data');
-            //TODO start the python process again
             //TODO send out success message
             childProcess.kill('SIGINT'); //TODO I want to be sure the process is killed
             childProcess = startPythonProcess();
-            fs.writeFileSync(`/home/chip/success-${new Date()}`, data);
+            fs.writeFileSync(`/home/chip/git-pull-success-${new Date()}`, data);
             return;
         }
     });
 });
 
-//TODO I want to be sure the process is started correctly
 function startPythonProcess() {
-    return require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
+    const childProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
+    childProcess.on('error', (error) => {
+        fs.writeFileSync(`/home/chip/child-process-error-${new Date()}`, error);
+    });
+    fs.writeFileSync(`/home/chip/child-process-started-${new Date()}`, 'child-process-started');
+    return childProcess;
 }
