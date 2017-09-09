295185067c65af59f9aae1362532f8a10c084f90
Jordan Last <jordan.michael.last@gmail.com>
2017-09-09 01:30:33 -0600

    use promises

--- piero-init.js
+++ piero-init.js
@@ -2,35 +2,37 @@ const schedule = require('node-schedule');
 const fs = require('fs');
 const fetch = require('isomorphic-fetch')
 
-const pythonProcess = startPythonProcess();
-
-schedule.scheduleJob({
-    second: 10
-}, () => {
-    pythonProcess.kill('SIGINT');
-    sendMessage(`START_UPDATE`, '');
-    // require('child_process').execSync('wget -qO- https://raw.githubusercontent.com/hornej/piero-init/master/piero-init.sh | bash');
+startPythonProcess().then((pythonProcess) => {
+    schedule.scheduleJob({
+        second: 10
+    }, () => {
+        pythonProcess.kill('SIGINT');
+        sendMessage(`START_UPDATE`, '');
+        // require('child_process').execSync('wget -qO- https://raw.githubusercontent.com/hornej/piero-init/master/piero-init.sh | bash');
+    });
 });
 
 function startPythonProcess() {
-    sendMessage(`START_PYTHON_PROCESS`, '');
+    return new Promise((resolve, reject) => {
+        sendMessage(`START_PYTHON_PROCESS`, '').then(() => {
+            const pythonProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
+            pythonProcess.on('error', (error) => {
+                sendMessage(`PYTHON_PROCESS_ERROR`, error.toString());
+            });
+            pythonProcess.stdout.on('data', (data) => {
+                sendMessage(`PYTHON_PROCESS_STDOUT`, data.toString());
+            });
+            pythonProcess.stderr.on('data', (data) => {
+                sendMessage(`PYTHON_PROCESS_STDERR`, data.toString());
+            });
 
-    const pythonProcess = require('child_process').spawn('python', ['/home/chip/piero/chip_scan.py']);
-    pythonProcess.on('error', (error) => {
-        sendMessage(`PYTHON_PROCESS_ERROR`, error.toString());
+            resolve(pythonProcess);
+        });
     });
-    pythonProcess.stdout.on('data', (data) => {
-        sendMessage(`PYTHON_PROCESS_STDOUT`, data.toString());
-    });
-    pythonProcess.stderr.on('data', (data) => {
-        sendMessage(`PYTHON_PROCESS_STDERR`, data.toString());
-    });
-
-    return pythonProcess;
 }
 
 function sendMessage(objectName, objectContents) {
-    fetch(`https://piero-test.s3.amazonaws.com/${objectName}-${new Date().toString().split(' ').join('')}`, {
+    return fetch(`https://piero-test.s3.amazonaws.com/${objectName}-${new Date().toString().split(' ').join('')}`, {
         method: 'put',
         headers: {
             'Content-Type': 'application/json'
