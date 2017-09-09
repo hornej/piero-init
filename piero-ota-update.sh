848b4f71c1aa2cb3399a3a3e2ba9dbcd85c0f4bb
Jordan Last <jordan.michael.last@gmail.com>
2017-09-09 13:43:18 -0600

    push the npm cache fix

--- piero-ota-update.sh
+++ piero-ota-update.sh
@@ -7,6 +7,11 @@ echo "Cloning piero-ota-update repository"
 rm -rf piero-ota-update
 git clone https://github.com/hornej/piero-ota-update.git
 
+#TODO Clearnig the npm cache should not be necessary once using a version of Node with npm 5 or greater
+# Clearing the cache will ensure that the cache is not in an inconsistent state because of unexpected interruptions during npm installs
+echo "Clearing npm cache"
+npm cache clean
+
 echo "Installing npm dependencies"
 cd piero-ota-update
 npm install
