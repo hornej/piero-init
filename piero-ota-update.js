f1ebdf8a20eb6ec11e635134718153a09f65073b
Jordan Last <jordan.michael.last@gmail.com>
2017-09-11 13:27:12 -0600

    schedule

--- piero-ota-update.js
+++ piero-ota-update.js
@@ -14,7 +14,7 @@ sendMessage(`START_PYTHON_PROCESS`, '')
             .then(() => {
                 schedule.scheduleJob({
                     hour: 15,
-                    minute: 30
+                    minute: 33
                 }, () => {
                     performUpdate(pythonProcess);
                 });
